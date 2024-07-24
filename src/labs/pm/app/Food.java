79080d3b12860ecfb77ce6ae1166e419a26f6d6e
Bhawuk Arora <bhawukarora042@gmail.com>
2024-07-24 10:24:39 +0530

    Initial Commit

---
+++ src/labs/pm/app/Food.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2024 Deepika
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import labs.pm.data.Product;
+
+/**
+ *
+ * @author Deepika
+ */
+public class Food extends Product {
+
+    private LocalDate bestBefore;
+
+    public Food(int id, String name, BigDecimal price, LocalDate bestBefore) {
+        super(id, name, price);
+        this.bestBefore = bestBefore;
+    }
+
+    /**
+     * Get the value of bestBefore
+     *
+     * @return the value of bestBefore
+     */
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
+
+    @Override
+    public BigDecimal getDiscount() {
+        return (bestBefore.isEqual(LocalDate.now()))
+                ? super.getDiscount() : BigDecimal.ZERO;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "," + bestBefore;
+    }
+
+}
